664b1da1082d9b241c1fdfad98e839b388c681e0
starbugs <mail@tlensing.org>
2012-11-12 22:32:09 +0100

    ShaderPlayground wip

---
+++ tests-mac/ShaderPlayground/ShaderPlaygroundViewController.m
@@ -0,0 +1,54 @@
+//
+//  Copyright (C) 2012 Tobias Lensing, Marcus Tillmanns
+//  http://icedcoffee-framework.org
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//  of the Software, and to permit persons to whom the Software is furnished to do
+//  so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+#import "ShaderPlaygroundViewController.h"
+
+@implementation ShaderPlaygroundViewController
+
+@synthesize sprite = _sprite;
+
+- (void)setUpScene
+{
+    ICScene *scene = [ICScene scene];
+    [self runWithScene:scene];
+    
+    self.sprite = [ICSprite sprite];
+    self.sprite.size = kmVec3Make(300, 200, 0);
+    [scene addChild:self.sprite];
+}
+
+- (void)setShaderSource:(NSString *)fsh
+{
+    ICShaderFactory *factory = [[ICShaderCache currentShaderCache] shaderFactory];
+    NSString *vsh = [factory vertexShaderStringForKey:ICShaderPositionTextureColor];
+    ICAnimatedShaderProgram *program = [ICAnimatedShaderProgram shaderProgramWithName:@"CustomShader"
+                                                                   vertexShaderString:vsh
+                                                                 fragmentShaderString:fsh];
+    [program addAttribute:ICAttributeNamePosition index:0];
+    [program addAttribute:ICAttributeNameColor index:1];
+    [program addAttribute:ICAttributeNameTexCoord index:2];
+    [program link];
+    [self.sprite setShaderProgram:program];
+}
+
+@end
---
+++ tests-mac/ShaderPlayground/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ShaderPlayground
+//
+//  Created by Tobias Lensing on 11/11/12.
+//  Copyright (c) 2012 Tobias Lensing. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
