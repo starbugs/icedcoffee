20903cacbdd8b3838bb50a453321e16c77f5e5cb
starbugs <mail@tlensing.org>
2012-12-21 17:46:22 +0100

    Restored missing files from merge

---
+++ icedcoffee/ICGlyphCache.m
@@ -0,0 +1,53 @@
+//
+//  Copyright (C) 2012 Tobias Lensing, Marcus Tillmanns
+//  http://icedcoffee-framework.org
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//  of the Software, and to permit persons to whom the Software is furnished to do
+//  so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+#import "ICGlyphCache.h"
+#import "ICOpenGLContext.h"
+
+@implementation ICGlyphCache
+
++ (id)currentGlyphCache
+{
+    ICOpenGLContext *openGLContext = [ICOpenGLContext currentContext];
+    NSAssert(openGLContext != nil, @"No OpenGL context available for current native OpenGL context");
+    ICGlyphCache *glyphCache = openGLContext.glyphCache;
+    if (!glyphCache) {
+        glyphCache = openGLContext.glyphCache = [[[ICGlyphCache alloc] init] autorelease];
+    }
+    return glyphCache;
+}
+
+- (id)init
+{
+    if ((self = [super init])) {
+        _textures = [[NSMutableArray alloc] initWithCapacity:1];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
