a8c94a65da17efb8f336e4e52f35c1d21b02be24
starbugs <mail@tlensing.org>
2012-12-21 21:55:23 +0100

    RectangleBinPack with minor modifications for icedcoffee (_DEBUG => DEBUG)

---
+++ icedcoffee/ICTextureGlyph.h
@@ -0,0 +1,13 @@
+//
+//  ICTextureGlyph.h
+//  icedcoffee-ios
+//
+//  Created by Tobias Lensing on 12/21/12.
+//  Copyright (c) 2012 Tobias Lensing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ICTextureGlyph : NSObject
+
+@end
---
+++ icedcoffee/ICTextureGlyph.m
@@ -0,0 +1,13 @@
+//
+//  ICTextureGlyph.m
+//  icedcoffee-ios
+//
+//  Created by Tobias Lensing on 12/21/12.
+//  Copyright (c) 2012 Tobias Lensing. All rights reserved.
+//
+
+#import "ICTextureGlyph.h"
+
+@implementation ICTextureGlyph
+
+@end
